98899f56deed561545f917e6013ba909b34afba9
Christian Rasmussen <Mc_heer@hotmail.com>
2022-08-04 12:50:26 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
 import express from 'express';
+import { Prisma, PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient();
 const app = express();
 const port = 8080; // default port to listen
+
+app.use(express.json());
 
 // define a route handler for the default home page
 app.get( '/', ( req, res ) => {
     res.send( 'Hello world!' );
 } );
 
+app.post('/create/embeddedController', async (req, res) => {
+    const {name, location} = req.body;
+
+    const result = await prisma.embeddedController.create({
+        data: {
+            name: name,
+            location: location,
+        }
+    })
+
+    res.json(result);
+});
 // start the Express server
 app.listen( port, () => {
     // tslint:disable-next-line:no-console
