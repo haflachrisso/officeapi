6ff6d9a4fba81ec6a6634a2a95e2c170dfdb0bbf
Christian Rasmussen <Mc_heer@hotmail.com>
2022-08-04 13:17:16 +0200

    get embedded and changed apis

--- src/index.ts
+++ src/index.ts
@@ -12,18 +12,32 @@ app.get( '/', ( req, res ) => {
     res.send( 'Hello world!' );
 } );
 
-app.post('/create/embeddedController', async (req, res) => {
+app.post('/api/embeddedController/create', async (req, res) => {
     const {name, location} = req.body;
+    try {
 
-    const result = await prisma.embeddedController.create({
-        data: {
-            name: name,
-            location: location,
-        }
-    })
-
-    res.json(result);
+        const result = await prisma.embeddedController.create({
+            data: {
+                name: name,
+                location: location,
+            }
+        })
+        
+        res.json(result);
+    } catch (e) {
+        console.log(e);
+    }
 });
+
+app.get('/api/embeddedControllers', async (req, res) => {
+    try {
+        const embeddedControllers = await prisma.embeddedController.findMany();
+        res.json(embeddedControllers);
+    } catch(e) {
+        console.log(e);
+    }
+});
+
 // start the Express server
 app.listen( port, () => {
     // tslint:disable-next-line:no-console
