024df565bc69fe20e0fdd20890918993c9d1efb9
Christian Rasmussen <Mc_heer@hotmail.com>
2022-08-12 09:02:21 +0200

    added new model and new api

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import { Prisma, PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient();
 const app = express();
 const port = 8080; // default port to listen
-
+app.use(cors());
 app.use(express.json());
 
 // define a route handler for the default home page
@@ -59,12 +60,12 @@ app.post('/api/tempMeasurement/create', async (req, res) => {
         console.log(e);
     }
 });
-//hey
+// hey
 app.post('/api/tempMeasurement/:temp/:id', async (req, res) => {
     const {temp, id} = req.params;
 
-    const tempCelcius = parseFloat(temp);
-    const controllerId = parseInt(id);
+    const tempCelcius = Number(temp);
+    const controllerId = Number(id);
     try {
         const result = await prisma.tempMeasurements.create({
             data: {
@@ -80,7 +81,50 @@ app.post('/api/tempMeasurement/:temp/:id', async (req, res) => {
     }
 });
 
-app.get('/api/tempMeasurements', async (req, res) => {
+app.get('/api/tempMeasurements/:number?', async (req, res) => {
+
+    const number = req.params.number;
+
+    const numberOfMeasurements = number.length > 0 ? Number(number) : undefined;
+
+
+    try {
+        const tempMeasurements = await prisma.tempMeasurements.findMany({
+            take: -numberOfMeasurements
+        });
+        // const allUsers = await prisma.user.findMany({
+        //     include: {
+        //       posts: true,
+        //       profile: true,
+        //     },
+        //   })
+        res.json(tempMeasurements);
+    } catch(e) {
+        // tslint:disable-next-line:no-console
+        console.log(e);
+    }
+});
+
+app.post('/api/humMeasurement/:hum/:id', async (req, res) => {
+    const {hum, id} = req.params;
+
+    const humPercent = Number(hum);
+    const controllerId = Number(id);
+    try {
+        const result = await prisma.humMeasurements.create({
+            data: {
+                humPercent,
+                controllerId,
+            }
+        })
+        res.json(result);
+    } catch (e) {
+        // tslint:disable-next-line:no-console
+        console.log(e);
+    }
+});
+
+app.get('/api/humMeasurements', async (req, res) => {
     try {
         const tempMeasurements = await prisma.tempMeasurements.findMany();
         // const allUsers = await prisma.user.findMany({
